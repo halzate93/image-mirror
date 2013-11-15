b86efe19bd48f0f071358d3791ec238fba03c1c7
Hector Alzate <halzate93@gmail.com>
2013-11-15 11:50:14 -0500

    Downloaded helps from the monitor

--- setup.sh
+++ setup.sh
@@ -1,1 +1,18 @@
-apt-get install nasm
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+#nasm
+sudo apt-get install nasm
+#opencv dependencies
+sudo apt-get install build-essential libgtk2.0-dev libjpeg-dev libtiff4-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen2-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev libqt4-dev libqt4-opengl-dev sphinx-common texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev
+#download and unzip opencv
+wget http://downloads.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.2/OpenCV-2.4.2.tar.bz2
+tar -xvf OpenCV-2.4.2.tar.bz2
+rm -f OpenCV-2.4.2.tar.bz2
+cd OpenCV-2.4.2
+#generar el makefile
+mkdir build
+cd build
+cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+make
+sudo make install
