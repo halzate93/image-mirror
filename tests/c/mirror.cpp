ec4c7878835f7b61c855e533038c20b2776d8d15
castillobg <castillobgr@gmail.com>
2013-11-17 20:23:05 -0800

    Basic cpp methods for mat iteration

--- tests/c/mirror.cpp
+++ tests/c/mirror.cpp
@@ -14,30 +14,23 @@ int main( int argc, char** argv )
     printf( "No image data \n" );
     return -1;
   }
-  
-  int i,j,r,g,b;
+  /*
   for(int i = 0; i < img.rows; i++){
     for(int j = 0; j < img.cols / 2; j++){
-      input[img.cols * i + (img.rows - 2 - j)] = input[img.cols * i + j];
-      input[img.cols * i + (img.rows - 1 - j)] = input[img.cols * i + j + 1];
-      input[img.cols * i + (img.rows - j)] = input[img.cols * i + j + 2];
-      
-      //img.at<Vec3b>(j, img.cols - 1 - i)[0] = img.at<Vec3b>(j,i)[0];  //turn the pixel value @ (k,i) to yellow (0,255,255)
-      //img.at<Vec3b>(j, img.cols - 1 - i)[1] = img.at<Vec3b>(j,i)[0];  
-      //img.at<Vec3b>(j, img.cols - 1 - i)[2] = img.at<Vec3b>(j,i)[0];
+      img.at<Vec3b>(i, img.cols - 1 - j) = img.at<Vec3b>(i,j);
+    }
+  }*/
 
-      //b = input[img.step * j + i ] ;
-      //g = input[img.step * j + i + 1];
-      //r = input[img.step * j + i + 2];
+  //Some other way, with the image pointer
+  int ch = img.channels();
+  for(int i = 0; i < img.rows; i++){
+    for(int j = 0; j < (img.cols / 2); j++){
+      for(int k = 0; k < ch; k++){
+	input[img.cols * i * ch + (img.cols - j) * ch + k] = input[img.cols * i * ch + j * ch + k];
+      }
     }
   }
-  //for(int i = 0; i < image.rows / 2; i++){
-    //for(int j = 0; j < image.cols; j++){
-      //image.at(image.rows - 1 - i, j)[0] = image.at(i, j)[0];
-      //image.at(image.rows - 1 - i, j)[1] = image.at(i, j)[1];
-      //image.at(image.rows - 1 - i, j)[2] = image.at(i, j)[2];
-      //}
-    //  }
+  
   namedWindow( "Display Image", CV_WINDOW_AUTOSIZE );
   imshow( "Display Image", img );
 
---
+++ tests/c/readme.txt
@@ -0,0 +1,5 @@
+Para compilar:
+cmake .
+make
+
+El ejecutable recibe como argumento el path de una imagen.
